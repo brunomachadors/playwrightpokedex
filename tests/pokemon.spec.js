17d8cadb5efc45c16063e4b9a186c111dea98944
bruno.machado <bruno.machado@avios.com>
2024-06-26 18:29:13 +0100

    Playwright first steps and adding a pokedex test

---
+++ tests/pokemon.spec.js
@@ -0,0 +1,23 @@
+const { test, expect } = require('@playwright/test');
+
+test('Select Pokémon Gengar', async ({ page }) => {
+  await page.goto('https://brunomachadors.github.io/pokedex/');
+
+  await expect(page.getByLabel('POKÉMON Button')).toBeVisible();
+  await page.getByLabel('POKÉMON Button').click();
+  await expect(page.getByLabel('Select Pokemon')).toBeVisible();
+  await expect(
+    page.getByLabel('Pokedex Landscape Container').locator('div').nth(2)
+  ).toBeVisible();
+  await expect(page.getByLabel('TYPES Button')).toBeVisible();
+  await expect(page.getByLabel('ITEMS Button')).toBeVisible();
+  await expect(page.getByLabel('REGIONS Button')).toBeVisible();
+  await expect(page.getByLabel('FOSSILS Button')).toBeVisible();
+  await expect(
+    page.getByLabel('Select the pokemon gengar', { exact: true })
+  ).toBeVisible();
+
+  await page.getByLabel('Select the pokemon gengar', { exact: true }).click();
+
+  //await expect(page.getByLabel('Open image of gengar')).toBeVisible();
+});
