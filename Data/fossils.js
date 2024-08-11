758844a470a9659cb34667826bf461def42d70a1
TatianaHonda58 <tatianamitiko58@gmail.com>
2024-08-11 16:38:51 -0300

    adjustments read me file

---
+++ Data/fossils.js
@@ -0,0 +1,70 @@
+export const FOSSILS = [
+    {
+        "name": "#94 - honey ",
+        "information": "No short effect available",
+    },
+    {
+        "name": "#99 - root-fossil",
+        "underground": "Can be revived into a Lileep.",
+    },
+    {
+        "name": "#100 - claw-fossil",
+        "underground": "Can be revived into an Anorith.",
+    }, 
+    {
+        "name": "#101 - helix-fossil",
+        "underground": "Can be revived into an Omanyte.",
+    },
+    {
+        "name": "#102 - dome-fossil",
+        "underground": "Can be revived into a Kabuto.",
+    },
+    {
+        "name": "#103 - old-amber",
+        "underground": "Can be revived into an Aerodactyl.",
+    },
+    {
+        "name": "#104 - armor-fossil",
+        "underground": "Can be revived into a Shieldon.",
+    },
+    {
+        "name": "#105 - skull-fossil",
+        "underground": "Can be revived into a Cranidos.",
+    },
+    {
+        "name": "#111 - odd-keystone",
+        "underground": "Use on the tower on Route 209 to encounter Spiritomb if you have at least 32 Underground greetings.",
+    },
+    {
+        "name": "#613 - cover-fossil",
+        "information": "Can be revived into a tirtouga.",
+    },
+    {
+        "name": "#614 - plume-fossil",
+        "information": "Can be revived into a archen.",
+    },
+    {
+        "name": "#729 - jaw-fossil",
+        "information": "Can be revived into a Tyrunt.",
+    },
+    {
+        "name": "#730 - sail-fossil",
+        "information": "Can be revived into a Amaura.",
+    },
+    {
+        "name": "#1163 - fossilized-bird",
+        "information": "No short effect available",
+    },
+    {
+        "name": "#1164 - fossilized-fish",
+        "information": "No short effect available",
+    },
+    {
+        "name": "#1165 - fossilized-drake",
+        "information": "No short effect available",
+    },
+    {
+        "name": "#1166 - fossilized-dino",
+        "information": "No short effect available",
+    }
+]
