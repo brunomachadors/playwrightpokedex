39b4fc18de710803038f59adec9a760e9943f68c
TatianaHonda58 <tatianamitiko58@gmail.com>
2024-08-12 05:23:35 -0300

    creation of fossils.js in Data

---
+++ Data/fossils.js
@@ -0,0 +1,71 @@
+export const FOSSILS = [
+  {
+    name: '#94 - honey ',
+    underground: 'No short effect available',
+  },
+  {
+    name: '#99 - root-fossil',
+    underground: 'Can be revived into a Lileep.',
+  },
+  {
+    name: '#100 - claw-fossil',
+    underground: 'Can be revived into an Anorith.',
+  },
+  {
+    name: '#101 - helix-fossil',
+    underground: 'Can be revived into an Omanyte.',
+  },
+  {
+    name: '#102 - dome-fossil',
+    underground: 'Can be revived into a Kabuto.',
+  },
+  {
+    name: '#103 - old-amber',
+    underground: 'Can be revived into an Aerodactyl.',
+  },
+  {
+    name: '#104 - armor-fossil',
+    underground: 'Can be revived into a Shieldon.',
+  },
+  {
+    name: '#105 - skull-fossil',
+    underground: 'Can be revived into a Cranidos.',
+  },
+  {
+    name: '#111 - odd-keystone',
+    underground:
+      'Use on the tower on Route 209 to encounter Spiritomb if you have at least 32 Underground greetings.',
+  },
+  {
+    name: '#613 - cover-fossil',
+    underground: 'Can be revived into a tirtouga.',
+  },
+  {
+    name: '#614 - plume-fossil',
+    underground: 'Can be revived into a archen.',
+  },
+  {
+    name: '#729 - jaw-fossil',
+    underground: 'Can be revived into a Tyrunt.',
+  },
+  {
+    name: '#730 - sail-fossil',
+    underground: 'Can be revived into a Amaura.',
+  },
+  {
+    name: '#1163 - fossilized-bird',
+    underground: 'No short effect available',
+  },
+  {
+    name: '#1164 - fossilized-fish',
+    underground: 'No short effect available',
+  },
+  {
+    name: '#1165 - fossilized-drake',
+    underground: 'No short effect available',
+  },
+  {
+    name: '#1166 - fossilized-dino',
+    underground: 'No short effect available',
+  },
+];
